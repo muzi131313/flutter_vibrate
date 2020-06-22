d4e438af2b918be12acbe3588aac16b5a5f51164
fps-static <liyanfeng@zhibenyuan.cc>
2020-06-22 20:22:47 +0800

    fix: iOS引入问题修复

--- ios/Classes/VibratePlugin.m
+++ ios/Classes/VibratePlugin.m
@@ -1,5 +1,5 @@
 #import "VibratePlugin.h"
-#import <flutter_vibrate/flutter_vibrate-Swift.h>
+#import "flutter_vibrate-Swift.h"
 
 @implementation VibratePlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
